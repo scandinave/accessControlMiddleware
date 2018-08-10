df8350da092f5fb72e4afa195b7a102d92765b00
Romain LE BARO <ninja@MacBook-Pro-de-Romain.local>
2018-08-10 23:34:44 +0200

    Add git ignore and comment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ class AccessControlMiddleware {
     }
 
     check() {
-        const userAuthorizations = ...;
+        //const userAuthorizations = ...;
         if (this.hasRelatedToken()) {
             this.checkRelated();
         } else if (this.isMultipleResources()) {
@@ -38,4 +38,54 @@ class AccessControlMiddleware {
             }
         }
     }
+
+    /**
+     * Check that the user have correct the dynamic authorization
+     * in it's access token.
+     */
+    checkDynamic() {
+
+    }
+
+    /**
+     * Checl that the user have the correct genereic authorization in it' access token.
+     */
+    checkGeneric() {
+
+    }
+
+    /**
+     * Check that the user have the specific or dynamic authorization
+     * in it's access token.
+     */
+    checkHybride() {
+
+    }
+
+    /**
+     * Checl that the user have the correct specific authorization in it's access token
+     */
+    checkSpecific() {
+
+    }  
+
+    /**
+     * Check if the client send a resource related token that cause the request 
+     * to bypass others checks.
+     */
+    hasRelatedToken() {
+
+    }
+
+    /**
+     * Return true if the access is wanted to a set of resources or a specifics resources.
+     */
+    isMultipleResources() {
+
+    }
+
+    getUserAuthorizationType() {
+
+    }
+
 }
