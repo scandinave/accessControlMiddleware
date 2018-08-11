ce5a6b7cd20bebe94338efc4e09a8347c0830a22
scandinave <romain.lebaro@protonmail.com>
2018-08-11 20:12:43 +0200

    Feature: Add hasRelatedToken, checkRelated and isMultipleResources method

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
+"use strict";
+const Common = require("./common");
 class AccessControlMiddleware {
 
-    constructor() {
-
+    constructor(secret) {
+        this.secret = secret;
     }
 
-    check() {
-        //const userAuthorizations = ...;
-        if (this.hasRelatedToken()) {
-            this.checkRelated();
-        } else if (this.isMultipleResources()) {
-            switch (this.getUserAuthorizationType()) {
-                case "generic":
-                    this.checkGeneric();
-                    break;
-                case "dynamic":
-                    this.checkHybride();
-                    break;
-                case "specific":
-                    this.checkHybride();
-                    break;
-                default:
-                    break;
-            }
-        } else {
-            switch (this.getUserAuthorizationType()) {
-                case "generic":
-                    this.checkGeneric();
-                    break;
-                case "dynamic":
-                    this.checkDynamic();
-                    break;
-                case "specific":
-                    this.checkSpecific();
-                    break;
-                default:
-                    break;
+    check({ context } = {}) {
+        return (req, res, next) => {
+            //const userAuthorizations = ...;
+            if (this.hasRelatedToken(req.body)) {
+                this.checkRelated();
+            } else if (this.isMultipleResources(context)) {
+                switch (this.getUserAuthorizationType()) {
+                    case "generic":
+                        this.checkGeneric();
+                        break;
+                    case "dynamic":
+                        this.checkHybride();
+                        break;
+                    case "specific":
+                        this.checkHybride();
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                switch (this.getUserAuthorizationType()) {
+                    case "generic":
+                        this.checkGeneric();
+                        break;
+                    case "dynamic":
+                        this.checkDynamic();
+                        break;
+                    case "specific":
+                        this.checkSpecific();
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
@@ -67,21 +71,54 @@ class AccessControlMiddleware {
      */
     checkSpecific() {
 
-    }  
+    }
 
     /**
      * Check if the client send a resource related token that cause the request 
      * to bypass others checks.
+     * @param body The request body.
+     * @return True if a related token is present in the request body, false otherwise
      */
-    hasRelatedToken() {
+    hasRelatedToken(body) {
+        if (Common.isEmpty(body)) {
+            throw new Error("Missing parameter : body");
+        }
+        return Common.isNotEmpty(body.relatedToken);
+    }
+
+    /**
+     * Check the resource related token to ensure the client can access the resource.
+     * 
+     */
+    checkRelated({ token, resource } = {}) {
+        let countError = 0;
+        if (Common.isEmpty(token)) {
+            countError++;
+        }
+        if (Common.isEmpty(resource)) {
+            countError += 2;
+        }
+        switch (countError) {
+            case 1:
+                throw new Error("Missing parameter : token");
+            case 2:
+                throw new Error("Missing parameter : resource");
+            case 3:
+                throw new Error("Missing parameters : token, resource");
+            default:
+                const payload = Common.verifyToken(token, "MySecret", "JWT")
+                return payload.grants.resource === resource;
+        }
 
     }
 
     /**
      * Return true if the access is wanted to a set of resources or a specifics resources.
+     * @param context The context of the request. Can be empty for multiple resources (eg: findAll)
+     * @return True if a context is present and false otherwise.
      */
-    isMultipleResources() {
-
+    isMultipleResources(context = {}) {
+        return Common.isNotEmpty(context);
     }
 
     getUserAuthorizationType() {
@@ -89,3 +126,5 @@ class AccessControlMiddleware {
     }
 
 }
+
+module.exports = AccessControlMiddleware;
