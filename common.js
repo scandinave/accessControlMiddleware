d24a87c6c9bd5d1b4f1ff8a241cd753c296fe63f
scandinave <romain.lebaro@protonmail.com>
2018-08-11 20:12:43 +0200

    Feature: Add hasRelatedToken, checkRelated and isMultipleResources method

---
+++ common.js
@@ -0,0 +1,55 @@
+"use strict";
+const jwt = require("jsonwebtoken");
+/**
+ * Common function to used inside other classes.
+ */
+class Common {
+    /**
+       * Check if an elem is empty. This method check for null or undefined value.
+       * If elem is an array, also check for size greater than 0.
+       * @param {*} elem The elem to check.
+       * @return {boolean} true if, the elem is empty, false otherwise.
+       */
+    static isEmpty(elem) {
+        if (Array.isArray(elem) && Object.keys(elem).length === 0) {
+            return true;
+        }
+
+        if (elem instanceof Object && Object.keys(elem).length === 0) {
+            return true;
+        }
+
+        return typeof elem === "undefined" || elem === null || elem === "";
+    }
+
+    /**
+    * Check if an elem is not empty. This method check for null or undefined value.
+    * If elem is an array, also check for size greater than 0.
+    * @param {*} elem The elem to check.
+    * @return {boolean} true if, the elem is not empty, false otherwise.
+    */
+    static isNotEmpty(elem) {
+        return !Common.isEmpty(elem);
+    }
+
+    /**
+   * Verify hat the resource token is valid
+   * @param {string} token The token to verify
+   * @param {string} secret The secret used to sign the token
+   * @param {string} format The token format (eg: JWT, Bearer ...)
+   * @return {Object} The decode token data
+   */
+    static verifyToken(token, secret, format) {
+        if (token.startsWith(format)) {
+            token = token.slice(format.length + 1);
+        }
+        try {
+            return jwt.verify(token, secret);
+
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+}
+
+module.exports = Common;
---
+++ test/app.test.js
@@ -0,0 +1,63 @@
+const chai = require("chai");
+const expect = chai.expect;
+chai.use(require("chai-http"));
+chai.use(require("chai-like"));
+chai.use(require("chai-things"));
+chai.use(require("chai-sorted"));
+chai.use(require("chai-match"));
+const AccessControlMiddleware = require("../app");
+const acm = new AccessControlMiddleware("MySecret");
+const jwt = require("jsonwebtoken");
+let authorizationToken;
+describe("AccessControlMiddleware", () => {
+    before(done => {
+        authorizationToken = jwt.sign({}, "MySecret", {
+            expiresIn: 10080 // in seconds
+        });
+
+        done();
+    })
+    describe("hasRelatedToken", () => {
+        it("should return false when no token is provided in request body", () => {
+            expect(acm.hasRelatedToken({ body: "Foo" })).to.be.false;
+        });
+        it("should return true when token is provided in request body", () => {
+            const relatedToken = jwt.sign({}, "MySecret", {
+                expiresIn: 10080 // in seconds
+            });
+            expect(acm.hasRelatedToken({ relatedToken })).to.be.true;
+        });
+
+        it("should throw an exception when invoking method without argument", () => {
+            expect(acm.hasRelatedToken).to.throw("Missing parameter : body")
+        });
+    });
+
+    describe("checkRelated", () => {
+        it("should return true when the token provided is not valid", () => {
+            const relatedToken = jwt.sign({ grants: { resource: "Foo" } }, "MySecret", {
+                expiresIn: 10080 // in seconds
+            });
+            expect(acm.checkRelated({ token: relatedToken, resource: "Bar" })).to.be.false;
+        });
+
+        it("should return false when the token provided is valid", () => {
+            const relatedToken = jwt.sign({ grants: { resource: "Foo" } }, "MySecret", {
+                expiresIn: 10080 // in seconds
+            });
+            expect(acm.checkRelated({ token: relatedToken, resource: "Foo" })).to.be.true;
+        });
+
+        it("should throw an exception when invoking method without argument", () => {
+            expect(acm.checkRelated).to.throw("Missing parameters : token, resource")
+        });
+    });
+    describe("isMultipleResources", () => {
+        it("should return false when an empty context is passed", () => {
+            expect(acm.isMultipleResources({})).to.be.false;
+        });
+        it("should return true when an context is passed", () => {
+            expect(acm.isMultipleResources({ source: "params", key: "fooId" })).to.be.true;
+        });
+    });
+});
